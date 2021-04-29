fc2f76edb64503675deb138aaa2c267452681187
EneRGy-79 <skril.iwan@gmail.com>
2021-04-29 23:02:23 +0300

    firstcommit

---
+++ src/test/java/fit/knu/ist/ta2/Ta2ApplicationTests.java
@@ -0,0 +1,13 @@
+package fit.knu.ist.ta2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ta2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
